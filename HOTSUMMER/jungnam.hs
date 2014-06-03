56d8a502ae8c3c8e9c1fdb01c51e7990ab59a57a
root <root@jungnam.(none)>
2014-06-03 12:08:50 +0900

    tokenizing funciton 변경

--- HOTSUMMER/jungnam.hs
+++ HOTSUMMER/jungnam.hs
@@ -4,13 +4,6 @@ main = do
   tstr <- getLine
   let tint = read tstr::Int
   replicateM_ tint (solve)
-
-tokenizeString :: String -> [Char] -> [[Char]]
-tokenizeString [] _ = [""]
-tokenizeString (c:cs) delim
-  | c `elem` delim = [] : rest
-  | otherwise = (c : head rest) : tail rest
-    where rest = tokenizeString cs delim
           
 f :: [String] -> [Int]
 f = map read
@@ -18,5 +11,5 @@ solve = do
   limit <- getLine
   let limitInt = read limit :: Int
   ustr <- getLine
-  let s = sum (f (tokenizeString ustr " "))
+  let s = sum (f (words ustr))
   if (s <= limitInt) then putStrLn "YES" else putStrLn "NO"
